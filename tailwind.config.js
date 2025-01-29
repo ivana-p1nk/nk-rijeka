9f788773f24c7b8392ff6527c13d6ef36b2694a1
ivana-p1nk <iicetina29@gmail.com>
2025-01-29 14:52:10 +0100

    Change font optimal size

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,14 @@ module.exports = {
             roboto: ['"Roboto Condensed"', 'sans-serif'],
           },
           fontSize: {
-            'h1-display': ['clamp(48px, 10vw, 96px)', { lineHeight: 'clamp(48px, 10vw, 96px)' }],  // font size, line height
-            'h2-display': ['clamp(40px, 10vw, 74px)', { lineHeight: 'clamp(40px, 10vw, 74px)' }],
-            'h1-normal': ['clamp(36px, 10vw, 52px)', { lineHeight: 'clamp(43px, 10vw, 62.4px)' }],
-            'h2-normal': ['clamp(32px, 10vw, 44px)', { lineHeight: 'clamp(38px, 10vw, 52.8px)' }],
-            'h3-normal': ['clamp(28px, 10vw, 38px)', { lineHeight: 'clamp(33px, 10vw, 45.6px)' }],
-            'h4-normal': ['clamp(24px, 10vw, 30px)', { lineHeight: 'clamp(29px, 10vw, 36px)' }],
-            'h5-normal': ['clamp(20px, 10vw, 24px)', { lineHeight: 'clamp(24px, 10vw, 28.8px)' }],
-            'h6-normal': ['clamp(18px, 10vw, 20px)', { lineHeight: 'clamp(22px, 10vw, 24px)' }],
+            'h1-display': ['clamp(48px, 5vw, 96px)', { lineHeight: 'clamp(48px, 5vw, 96px)' }],  // font size, line height
+            'h2-display': ['clamp(40px, 5vw, 74px)', { lineHeight: 'clamp(40px, 5vw, 74px)' }],
+            'h1-normal': ['clamp(36px, 5vw, 52px)', { lineHeight: 'clamp(43px, 5vw, 62.4px)' }],
+            'h2-normal': ['clamp(32px, 5vw, 44px)', { lineHeight: 'clamp(38px, 5vw, 52.8px)' }],
+            'h3-normal': ['clamp(28px, 5vw, 38px)', { lineHeight: 'clamp(33px, 5vw, 45.6px)' }],
+            'h4-normal': ['clamp(24px, 5vw, 30px)', { lineHeight: 'clamp(29px, 5vw, 36px)' }],
+            'h5-normal': ['clamp(20px, 5vw, 24px)', { lineHeight: 'clamp(24px, 5vw, 28.8px)' }],
+            'h6-normal': ['clamp(18px, 5vw, 20px)', { lineHeight: 'clamp(22px, 5vw, 24px)' }],
             'body1': ['18px', '25.2px'], // 18px font size, 25.2px line height
             'body2': ['16px', '22.4px'],
             'body3': ['14px', '19.6px'],
