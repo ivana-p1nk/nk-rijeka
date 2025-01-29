4840f8cacc1ab7e758423fb79af285fd629f0ab4
ivana-p1nk <iicetina29@gmail.com>
2025-01-29 16:28:34 +0100

    Custom Color Shades

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 
 <template>
-
 <h1 class="font-saira font-black text-xl underline">Headings font: Saira Condensed</h1>
 <br>
 <h1 class="font-saira font-bold text-h1-display">
@@ -129,6 +128,104 @@ Link2/Regular: Almost before we knew it, we had left the ground.
 <h1 class="font-roboto font-bold">Bold naslov (700)</h1>
 <h1 class="font-roboto font-extrabold">Extra-Bold naslov (800)</h1>
 <h1 class="font-roboto font-black">Black naslov (900)</h1>
+<br><br>
+
+<h1 class="font-saira font-black text-xl underline">Colors Shades</h1>
 <br>
 
+<!-- Div za boje -->
+<div class="flex"> <p class="font-roboto font-extrabold w-48 flex items-center">GRAY</p>
+  <div class="w-24 h-10 bg-gray-950"></div> 
+  <div class="w-24 h-10 bg-gray-900"></div> 
+  <div class="w-24 h-10 bg-gray-800"></div> <!-- w-24 = 100px, h-10 = 40px -->
+  <div class="w-24 h-10 bg-gray-700"></div> 
+  <div class="w-24 h-10 bg-gray-600"></div>
+  <div class="w-24 h-10 bg-gray-500"></div>
+  <div class="w-24 h-10 bg-gray-400"></div>
+  <div class="w-24 h-10 bg-gray-300"></div>
+  <div class="w-24 h-10 bg-gray-200"></div>
+  <div class="w-24 h-10 bg-gray-100"></div>
+  <div class="w-24 h-10 bg-gray-50"></div>
+</div>
+<br>
+<div class="flex"> <p class="font-roboto font-extrabold w-48 flex items-center">BLUE</p>
+  <div class="w-24 h-10 bg-blue-900"></div> 
+  <div class="w-24 h-10 bg-blue-800"></div> 
+  <div class="w-24 h-10 bg-blue-700"></div> 
+  <div class="w-24 h-10 bg-blue-600"></div>
+  <div class="w-24 h-10 bg-blue-500"></div>
+  <div class="w-24 h-10 bg-blue-400"></div>
+  <div class="w-24 h-10 bg-blue-300"></div>
+  <div class="w-24 h-10 bg-blue-200"></div>
+  <div class="w-24 h-10 bg-blue-100"></div>
+  <div class="w-24 h-10 bg-blue-50"></div>
+</div>
+<br>
+<div class="flex"> <p class="font-roboto font-extrabold w-48 flex items-center">NEUTRAL BLUE</p>
+  <div class="w-24 h-10 bg-neutralBlue-950"></div> 
+  <div class="w-24 h-10 bg-neutralBlue-900"></div> 
+  <div class="w-24 h-10 bg-neutralBlue-800"></div> 
+  <div class="w-24 h-10 bg-neutralBlue-700"></div> 
+  <div class="w-24 h-10 bg-neutralBlue-600"></div>
+  <div class="w-24 h-10 bg-neutralBlue-500"></div>
+  <div class="w-24 h-10 bg-neutralBlue-400"></div>
+  <div class="w-24 h-10 bg-neutralBlue-300"></div>
+  <div class="w-24 h-10 bg-neutralBlue-200"></div>
+  <div class="w-24 h-10 bg-neutralBlue-100"></div>
+  <div class="w-24 h-10 bg-neutralBlue-50"></div>
+</div>
+<br>
+<div class="flex"> <p class="font-roboto font-extrabold w-48 flex items-center">YELLOW</p>
+  <div class="w-24 h-10 bg-yellow-900"></div> 
+  <div class="w-24 h-10 bg-yellow-800"></div>
+  <div class="w-24 h-10 bg-yellow-700"></div> 
+  <div class="w-24 h-10 bg-yellow-600"></div>
+  <div class="w-24 h-10 bg-yellow-500"></div>
+  <div class="w-24 h-10 bg-yellow-400"></div>
+  <div class="w-24 h-10 bg-yellow-300"></div>
+  <div class="w-24 h-10 bg-yellow-200"></div>
+  <div class="w-24 h-10 bg-yellow-100"></div>
+  <div class="w-24 h-10 bg-yellow-50"></div>
+</div>
+<br>
+<div class="flex"> <p class="font-roboto font-extrabold w-48 flex items-center">GREEN</p>
+  <div class="w-24 h-10 bg-green-900"></div> 
+  <div class="w-24 h-10 bg-green-800"></div> 
+  <div class="w-24 h-10 bg-green-700"></div> 
+  <div class="w-24 h-10 bg-green-600"></div>
+  <div class="w-24 h-10 bg-green-500"></div>
+  <div class="w-24 h-10 bg-green-400"></div>
+  <div class="w-24 h-10 bg-green-300"></div>
+  <div class="w-24 h-10 bg-green-200"></div>
+  <div class="w-24 h-10 bg-green-100"></div>
+  <div class="w-24 h-10 bg-green-50"></div>
+</div>
+ 
+<br>
+<div class="flex"> <p class="font-roboto font-extrabold w-48 flex items-center">RED</p>
+  <div class="w-24 h-10 bg-red-900"></div> 
+  <div class="w-24 h-10 bg-red-800"></div> 
+  <div class="w-24 h-10 bg-red-700"></div> 
+  <div class="w-24 h-10 bg-red-600"></div>
+  <div class="w-24 h-10 bg-red-500"></div>
+  <div class="w-24 h-10 bg-red-400"></div>
+  <div class="w-24 h-10 bg-red-300"></div>
+  <div class="w-24 h-10 bg-red-200"></div>
+  <div class="w-24 h-10 bg-red-100"></div>
+  <div class="w-24 h-10 bg-red-50"></div>
+</div>
+<br>
+
+<div class="flex"> 
+<p class="font-roboto font-extrabold w-48 flex items-center">BLUE GRADIENT</p>
+<div class="w-80 h-10 bg-blue-gradient"></div> 
+</div>
+<br>
+<div class="flex"> 
+<p class="font-roboto font-extrabold w-48 flex items-center">DARK BLUE GRADIENT</p>
+<div class="w-80 h-10 bg-dark-blue-gradient"></div> 
+</div>
+
+
+<br><br>
 </template>
