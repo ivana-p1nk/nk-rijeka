2dfc4855f1d4ea10825ebe5fa180dc3771bfc1a1
ivana-p1nk <iicetina29@gmail.com>
2025-01-29 14:27:09 +0100

    Radials Typography Configuration

--- app.vue
+++ app.vue
@@ -1,10 +1,41 @@
 
 <template>
-  <h1 class="text-3xl font-bold underline text-amber-400">
-    Hello world!
-  </h1>
 
+<h1 class="font-saira font-black text-xl underline">Headings font: Saira Condensed</h1>
 <br>
+<h1 class="font-saira font-bold text-h1-display">
+  H1 DISPLAY: Almost before we knew it, we had left the ground.
+</h1>
+<br>
+<h2 class="font-saira font-bold text-h2-display">
+  H2 DISPLAY: Almost before we knew it, we had left the ground.
+</h2>
+<br>
+<h1 class="font-saira font-medium text-h1-normal">
+  H1: Almost before we knew it, we had left the ground.
+</h1>
+<br>
+<h2 class="font-saira font-medium text-h2-normal">
+  H2: Almost before we knew it, we had left the ground.
+</h2>
+<br>
+<h3 class="font-saira font-medium text-h3-normal">
+  H3: Almost before we knew it, we had left the ground.
+</h3>
+<br>
+<h4 class="font-saira font-semibold text-h3-normal">
+  H4: Almost before we knew it, we had left the ground.
+</h4> 
+<br>
+<h5 class="font-saira font-semibold text-h4-normal">
+  H5: Almost before we knew it, we had left the ground.
+</h5>
+<br>
+<h6 class="font-saira font-bold text-h5-normal">
+  H6: Almost before we knew it, we had left the ground.
+</h6>
+
+<br><br>
 
 <h1 class="font-saira font-thin">Thin naslov (100)</h1>
 <h1 class="font-saira font-extralight">Extra Light naslov (200)</h1>
@@ -15,41 +46,89 @@
 <h1 class="font-saira font-bold">Bold naslov (700)</h1>
 <h1 class="font-saira font-extrabold">Extra-Bold naslov (800)</h1>
 <h1 class="font-saira font-black">Black naslov (900)</h1>
+
 <br><br>
 
+<h1 class="font-roboto font-black text-xl underline">Body font: Roboto Condensed</h1>
+<br>
+<p class="font-robot font-normal text-body1">
+Body1/Regular: Almost before we knew it, we had left the ground.
+</p>
+<br>
+<p class="font-robot font-bold text-body1">
+Body1/Bold: Almost before we knew it, we had left the ground.
+</p>
+<br>
+<p class="font-robot font-normal text-body2">
+Body2/Regular: Almost before we knew it, we had left the ground.
+</p>
+<br>
+<p class="font-robot font-bold text-body2">
+Body2/Bold: Almost before we knew it, we had left the ground.
+</p>
+<br>
+<p class="font-robot font-normal text-body3">
+Body3/Regular: Almost before we knew it, we had left the ground.
+</p>
+<br>
+<p class="font-robot font-bold text-body3">
+Body3/Bold: Almost before we knew it, we had left the ground.
+</p>
+<br>
+<p class="font-robot font-normal text-body4">
+Body4/Regular: Almost before we knew it, we had left the ground.
+</p>
+<br>
+<p class="font-robot font-bold text-body4">
+Body4/Bold: Almost before we knew it, we had left the ground.
+</p>
+<br>
+<p class="font-robot font-bold text-label1">
+Label1/Bold: Almost before we knew it, we had left the ground.
+</p>
+<br>
+<p class="font-robot font-bold text-label2">
+Label2/Bold: Almost before we knew it, we had left the ground.
+</p>
+<br>
+<p class="font-robot font-bold text-button1">
+BUTTON1/BOLD: Almost before we knew it, we had left the ground.
+</p>
+<br>
+<p class="font-robot font-bold text-button2">
+BUTTON2/BOLD: Almost before we knew it, we had left the ground.
+</p>
+<br>
+<p class="font-robot font-bold text-button3">
+BUTTON3/BOLD: Almost before we knew it, we had left the ground.
+</p>
+<br>
+<p class="font-robot font-bold text-link1 underline">
+Link1/Bold: Almost before we knew it, we had left the ground.
+</p>
+<br>
+<p class="font-robot font-normal text-link1 underline">
+Link1/Regular: Almost before we knew it, we had left the ground.
+</p>
+<br>
+<p class="font-robot font-bold text-link2 underline">
+Link2/Bold: Almost before we knew it, we had left the ground.
+</p>
+<br>
+<p class="font-robot font-normal text-link2 underline">
+Link2/Regular: Almost before we knew it, we had left the ground.
+</p>
 
-<h1 class="font-saira">
-  H1 DISPLAY TITLE
-</h1>
-
-<h1 class="font-saira">
-  H1 TITLE
-</h1>
-
-<h2 class="font-saira">
-  H2 TITLE
-</h2>
-
-
-<h3 class="font-saira">
-  H3 TITLE
-</h3>
-
-<h4 class="font-saira">
-  H4 TITLE
-</h4> 
-
-<h5 class="font-saira">
-  H5 TITLE
-</h5>
-
-<h6 class="font-saira">
-  H6 TITLE
-</h6>
-
-
-<h1 class="font-saira font-bold text-h1-display leading-h1-display">Naslov za h1-display</h1>
-<h1 class="font-saira font-bold text-h1-normal leading-h1-normal">Naslov za h1-normal</h1>
-
+<br><br>
+<h1 class="font-roboto font-thin">Thin naslov (100)</h1>
+<h1 class="font-roboto font-extralight">Extra Light naslov (200)</h1>
+<h1 class="font-roboto font-light">Light naslov (300)</h1>
+<h1 class="font-roboto font-normal">Normal naslov (400)</h1>
+<h1 class="font-roboto font-medium">Medium naslov (500)</h1>
+<h1 class="font-roboto font-semibold">Semi-Bold naslov (600)</h1>
+<h1 class="font-roboto font-bold">Bold naslov (700)</h1>
+<h1 class="font-roboto font-extrabold">Extra-Bold naslov (800)</h1>
+<h1 class="font-roboto font-black">Black naslov (900)</h1>
+<br>
 
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,17 +7,30 @@ module.exports = {
     extend: {
         fontFamily:   {
             saira: ['"Saira Condensed"', 'sans-serif'],
+            roboto: ['"Roboto Condensed"', 'sans-serif'],
           },
           fontSize: {
-            'h1-display': ['clamp(48px, 10vw, 96px)'], 
-            'h1-normal': ['clamp(36px, 10vw, 52px)'], 
+            'h1-display': ['clamp(48px, 10vw, 96px)', { lineHeight: 'clamp(48px, 10vw, 96px)' }],  // font size, line height
+            'h2-display': ['clamp(40px, 10vw, 74px)', { lineHeight: 'clamp(40px, 10vw, 74px)' }],
+            'h1-normal': ['clamp(36px, 10vw, 52px)', { lineHeight: 'clamp(43px, 10vw, 62.4px)' }],
+            'h2-normal': ['clamp(32px, 10vw, 44px)', { lineHeight: 'clamp(38px, 10vw, 52.8px)' }],
+            'h3-normal': ['clamp(28px, 10vw, 38px)', { lineHeight: 'clamp(33px, 10vw, 45.6px)' }],
+            'h4-normal': ['clamp(24px, 10vw, 30px)', { lineHeight: 'clamp(29px, 10vw, 36px)' }],
+            'h5-normal': ['clamp(20px, 10vw, 24px)', { lineHeight: 'clamp(24px, 10vw, 28.8px)' }],
+            'h6-normal': ['clamp(18px, 10vw, 20px)', { lineHeight: 'clamp(22px, 10vw, 24px)' }],
+            'body1': ['18px', '25.2px'], // 18px font size, 25.2px line height
+            'body2': ['16px', '22.4px'],
+            'body3': ['14px', '19.6px'],
+            'body4': ['12px', '16.8px'],
+            'label1': ['14px', '14px'],
+            'label2': ['14px', '14px'],
+            'label2': ['12px', '14px'], 
+            'button1': ['15px', '1.2'], //120% = 1.2
+            'button2': ['14px', '1.2'],
+            'button3': ['13px', '1.2'],
+            'link1': ['14px', '1.2'],
+            'link2': ['12px', '1.2'],
           },
-          lineHeight: {
-            'h1-display': ['clamp(48px, 10vw, 96px)'],  
-            'h1-normal': ['clamp(43px, 10vw, 62.4px)'],  
-          },
-
-
     },
   },
   plugins: [],
