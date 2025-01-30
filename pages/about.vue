6820219a9250d16c198bd56de196a2fc9e24b303
ivana-p1nk <iicetina29@gmail.com>
2025-01-30 13:50:33 +0100

    NuxtLink

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-  <h2>ABOUT</h2>
+<div>
+  <header>
+    <nav>
+        <ul class="list-disc pl-5">
+            <li><NuxtLink to="/" class="text-blue-700 underline" >HOME</NuxtLink></li>
+            <li><NuxtLink to="/about" class="text-blue-700 underline" >ABOUT</NuxtLink></li>
+            <li><NuxtLink to="/theme-config" class="text-blue-700 underline">THEME CONFIG</NuxtLink></li>
+            <li><NuxtLink to="/products" class="text-blue-700 underline">PRODUCTS</NuxtLink></li>
+        </ul>
+    </nav>
+</header>
+<br>
+  <h2 class="font-bold">ABOUT</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam culpa, neque suscipit modi,
       perferendis nemo ipsam assumenda repudiandae tempora labore repellat nostrum? Repellat vero
       accusamus magni nulla voluptatum labore quis!</p>
