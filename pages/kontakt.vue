3bd190fdb0d695680d64c8d73a90083975a561d8
ivana-p1nk <iicetina29@gmail.com>
2025-03-31 15:53:52 +0200

    Kontakt page pt1.

---
+++ pages/kontakt.vue
@@ -0,0 +1,95 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+    <div class="bg-igraci">
+    <div class="container mx-auto pt-52 pb-5 px-5">
+
+        <h1 class="font-saira font-medium text-h1-normal text-gray-900 pb-20">
+            KONTAKT
+        </h1>
+        
+        <div class="flex">
+            <div class="w-1/4 flex flex-col bg-white p-12">
+                <img src="/assets/images/grb-rijeka.svg" alt="Grb NK Rijeka" class="max-w-[50px] mb-6">
+                <p class="font-roboto font-normal text-body2 ">
+                    <span class="font-semibold text-blue-600">Adresa: </span>HNK Rijeka, Rujevica 10<br>
+                    <span class="font-semibold text-blue-600">OIB: 38726608831</span><br>
+                    <span class="font-semibold text-blue-600">E-mail: <a href="mailto:shop@nk-rijeka.hr" class="underline">shop@nk-rijeka.hr</a></span>
+                </p>
+            </div>
+            <div class="w-3/4 relative">
+                <img src="/assets/images/map1.jpg" class="h-full object-cover">
+                <a href="https://www.google.com/maps/dir//Stadion+Rujevica+Rujevica+10+51000,+Rijeka+Hrvatska/@45.3478904,14.4022172,14z/data=!4m8!4m7!1m0!1m5!1m1!1s0x4764a6c6a45cd13f:0x698d0ad11598ac87!2m2!1d14.4022172!2d45.3478904?entry=ttu&g_ep=EgoyMDI1MDMyNS4xIKXMDSoJLDEwMjExNDUzSAFQAw%3D%3D"
+                    target="_blank" class="uppercase btn-primary large absolute bottom-6 left-6">
+                    <Icon name="majesticons:map-marker-area-line" class="mb-[-2px] mr-1" /> GOOGLE MAPS
+                </a>
+            </div>
+            
+        </div>
+
+
+        <h1 class="font-saira font-medium text-h1-normal text-gray-900 pt-14 pb-20">
+            FAN SHOP
+        </h1>
+
+        <div class="flex">
+            <div class="w-1/4 flex flex-col bg-white p-12">
+                <img src="/assets/images/grb-rijeka.svg" alt="Grb NK Rijeka" class="max-w-[50px] mb-6">
+                <h5 class="font-saira font-semibold text-h5-normal text-blue-900 pb-6">Fan Shop Stadion HNK Rijeka – Istok i zapad</h5>
+                <p class="font-roboto font-normal text-body2 ">
+                    <span class="font-semibold text-blue-600">Radno vrijeme: </span><br>
+                    Za dan domaće utakmice
+                </p>
+            </div>
+            <div class="w-3/4 relative flex flex-row">
+                <div>
+                    <img src="/assets/images/fanshop1.jpg" class="h-full object-cover">
+                    <a href="" target="_blank" class="uppercase btn-primary large absolute bottom-6 left-6">ISTOK</a>
+                </div>
+                <div>
+                    <img src="/assets/images/fanshop2.jpg" class="h-full object-cover">
+                    <a href="" target="_blank" class="uppercase btn-primary large absolute bottom-6 left-6">ZAPAD</a>
+                </div>
+               
+            </div>
+            
+        </div>
+        <br><br> <br><br> <br><br><br><br> <br><br> <br><br>
+        <div class="flex">
+            <div class="w-1/4 flex flex-col bg-white p-12">
+                <img src="/assets/images/grb-rijeka.svg" alt="Grb NK Rijeka" class="max-w-[50px] mb-6">
+                <p class="font-roboto font-normal text-body2 ">
+                    <span class="font-semibold text-blue-600">Adresa: </span>HNK Rijeka, Rujevica 10<br>
+                    <span class="font-semibold text-blue-600">OIB: 38726608831</span><br>
+                    <span class="font-semibold text-blue-600">E-mail: <a href="mailto:shop@nk-rijeka.hr" class="underline">shop@nk-rijeka.hr</a></span>
+                </p>
+            </div>
+            <div class="w-3/4 relative">
+                <img src="/assets/images/map1.jpg" class="h-full object-cover">
+                <a href="https://www.google.com/maps/dir//Stadion+Rujevica+Rujevica+10+51000,+Rijeka+Hrvatska/@45.3478904,14.4022172,14z/data=!4m8!4m7!1m0!1m5!1m1!1s0x4764a6c6a45cd13f:0x698d0ad11598ac87!2m2!1d14.4022172!2d45.3478904?entry=ttu&g_ep=EgoyMDI1MDMyNS4xIKXMDSoJLDEwMjExNDUzSAFQAw%3D%3D"
+                    target="_blank" class="uppercase btn-primary large absolute bottom-6 left-6">
+                    <Icon name="majesticons:map-marker-area-line" class="mb-[-2px] mr-1" /> GOOGLE MAPS
+                </a>
+            </div>
+            
+        </div>
+
+        
+
+    </div>
+</div>
+</template>
+
+<style>
+
+.bg-igraci {
+        background-image: url('/assets/images/bg-igraci.jpg');
+        background-repeat: no-repeat;
+        background-position: top;
+        background-size: contain;
+    }
+
+</style>
