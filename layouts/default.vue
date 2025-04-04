8d07a6bc1cae1bcc0d2673aea0f995c52e4e4c77
kristijanw <freelance.kristijan@gmail.com>
2025-04-04 11:26:30 +0200

    update layout footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,26 @@
-
 <script setup>
-import ErrorPage from '~/error.vue' 
-import { _white } from '#tailwind-config/theme/accentColor';
-import { ref, computed, toValue } from 'vue'
-import { useCartStore } from '~/composables/useCart';
+  import ErrorPage from '~/error.vue' 
+  import { _white } from '#tailwind-config/theme/accentColor';
+  import { ref, computed, toValue } from 'vue'
+  import { useCartStore } from '~/composables/useCart';
 
-const cartStore = useCartStore();
+  import Footer from '~/components/Footer.vue';
 
+  const cartStore = useCartStore();
 </script>
 
 
 <template>
-    <HeaderDesktop />
-    <HeaderMobile />
-    <template v-if="error">
-      <ErrorPage :error="error" />
-    </template>
-    <template v-else>
-      <slot />
-    </template>
-    <Footer />
+  <HeaderDesktop />
+  <HeaderMobile />
+
+  <template v-if="error">
+    <ErrorPage :error="error" />
   </template>
+  <template v-else>
+    <slot />
+  </template>
+  
+  <Footer />
+</template>
 
