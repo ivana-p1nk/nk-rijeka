9c7675c5002247fecaf9e48935482df60a41466a
kristijanw <freelance.kristijan@gmail.com>
2025-04-04 11:19:20 +0200

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,9 @@
 
 <script setup>
-import { useError } from '#app'
 import ErrorPage from '~/error.vue' 
 import { _white } from '#tailwind-config/theme/accentColor';
 import { ref, computed, toValue } from 'vue'
 import { useCartStore } from '~/composables/useCart';
-
-import Footer from '~/components/Footer.vue'
-import HeaderDesktop from '~/components/Header/Desktop.vue'
-import HeaderMobile from '~/components/Header/Mobile.vue'
 
 const cartStore = useCartStore();
 
