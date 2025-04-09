ac85309563b9ea625592996a5ed8d4304dbaea31
kristijanw <freelance.kristijan@gmail.com>
2025-04-09 13:50:49 +0200

    related products slider

--- components/Product/Related.vue
+++ components/Product/Related.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
-    import type { IProduct } from '~/types/product';
-    defineProps<{ product: IProduct; }>();
+	import type { IProduct } from '~/types/product';
+	defineProps<{ product: IProduct; }>();
 </script>
 
 <template>
-    <div class="py-10 related-products">
-      <h2 class="mb-6 text-2xl font-semibold text-gray-800">Related Products</h2>
-      <UCarousel 
-        v-slot="{ item }" 
-        :items="product.related_products"
-        :ui="{ item: 'basis-1/4' }"
-      >
-        <ProductCard :product="item" />
-      </UCarousel>
-    </div>
-  </template>
+	<div class="py-10 related-products">
+		<h2 class="mb-6 text-2xl font-semibold text-gray-800">Related Products</h2>
+		
+		<!-- <UCarousel 
+		v-slot="{ item }" 
+		:items="product.related_products"
+		:ui="{ item: 'basis-1/4' }"
+		>
+			<ProductCard :product="item" />
+		</UCarousel> -->
+
+		<Carousel :products="product.related_products" />
+	</div>
+</template>
