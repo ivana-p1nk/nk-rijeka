3ae475eb02629e7edc285266b8067b276775e1cb
ivana-p1nk <iicetina29@gmail.com>
2025-04-15 16:21:31 +0200

    Sidebar Cart content

--- components/Sidebar/Content.vue
+++ components/Sidebar/Content.vue
@@ -6,53 +6,71 @@
   
       <Offcanvas  :isOpen="show" @close="show = false">
         <div>
-            <div v-if="cartStore.cart_products.length > 0">
+            <div v-if="cartStore.cart_products.length > 0" class="flex flex-col gap-4">
                 <div
                 v-for="(item, index) in cartStore.cart_products"
                 :key="index"
-                class="border-b"
+                class="border-b pb-4"
                 >
-						<!-- PROIZVOD -->
-						<div class="flex gap-3">
-							<img :src="item.gallery[0]" class="object-contain w-24 h-24" />
-							<div class="flex flex-col gap-4">
-                                <p class="font-saira font-bold text-h6-normal text-blue-900">{{ item.title }}</p>
-                                <div class="flex flex-col gap-1">
-                                    <p class="font-roboto text-body3 text-gray-900"><span class="font-bold">MODEL:</span> TEST</p>
-                                    <p class="font-roboto text-body3 text-gray-900"><span class="font-bold">BROJ: </span>{{ item.variationName }}</p>
-							    </div>
-                            </div>
-                            <!-- DELETE -->
-						<div>
-							<UIcon name="heroicons:trash" class="h-6 ml-3 cursor-pointer w-7" @click="cartStore.removeCartProduct(item, item.variationId)" />
-						</div>
-                                
-						</div>
-						<!-- UKUPNO -->
-						<div>
-							{{ (item.orderQuantity * item.price).toFixed(2) }}
-						</div>
+                    <div class="grid gap-3" style="grid-template-columns: 96px 1fr 38px;">
+                        <!-- PRVI STUPAC: Slika proizvoda-->
+                        <img :src="item.gallery[0]" class="object-contain w-24 h-24" />
 
-						
-					</div>
-                    <p class="header-text text-red-300">{{ cartStore.totalPriceQuantity.total.toFixed(2) }} €</p>
+                        <!-- DRUGI STUPAC: Detalji o proizvodu -->
+                        <div class="flex flex-col gap-4">
+                            <p class="font-saira font-bold text-h6-normal text-blue-900">{{ item.title }}</p>
+                            <div class="flex flex-col gap-1">
+                                <p class="font-roboto text-body3 text-gray-900"><span class="font-bold">MODEL:</span> TEST</p>
+                                <p class="font-roboto text-body3 text-gray-900"><span class="font-bold">BROJ: </span>{{ item.variationName }}</p>
+                            </div>
+                            <div>
+                                <p class="font-bold">{{ item.orderQuantity }} x {{ item.price.toFixed(2).replace('.', ',') }} €</p>
+                            </div>
+                            <!-- KOLIČINA -->
+                            <div class="flex items-center space-x-2">
+                                <div class="flex items-center p-1 space-x-1">
+                                    <button class="btn-icon-secondary square-medium rounded-md" @click="cartStore.quantityDecrement(item, item.variationId)">
+                                        <UIcon name="heroicons:minus" />
+                                    </button>
+                                    <input class="bg-white border-blue-500 border-[1px] square-medium rounded-lg text-center" type="text" :value="item.orderQuantity" :v-model="item.orderQuantity"
+                                        disabled />
+                                    <button class="btn-icon-secondary square-medium rounded-md" @click="cartStore.addCartProduct(item, item.variationId)">
+                                        <UIcon name="heroicons:plus" />
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- TREĆI STUPAC: Gumb za brisanje proizvoda-->
+                        <div class="flex items-center justify-end">
+                            <button
+                            class="btn-icon-secondary circle-medium"
+                            @click="() => { cartStore.removeCartProduct(item, item.variationId); }"
+                            >
+                            <UIcon name="heroicons:trash" class="h-4 cursor-pointer w-4 text-red-600" />
+                            </button>
+                        </div>
+
+                    </div>
+					
 				</div>
+                <p class="">{{ cartStore.totalPriceQuantity.total.toFixed(2) }} €</p>
+			</div>
 				<div v-else class="text-center">
 					<p>Tvoja košarica je prazna</p>
 				</div>
-			</div>
-
-            
+		</div>
       </Offcanvas >
     </div>
-  </template>
+</template>
   
-  <script setup>
+<script setup>
   import Offcanvas  from '~/components/Sidebar/Offcanvas.vue'
   import { ref } from 'vue'
   import { useCartStore } from '~/composables/useCart';
-
-    const cartStore = useCartStore();
   
+  const cartStore = useCartStore();
   const show = ref(false)
-  </script>
+
+</script>
+
