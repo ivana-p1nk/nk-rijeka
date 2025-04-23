f7f03b1480c0e97970b80e9fe2fdab3f959ad7e3
kristijanw <freelance.kristijan@gmail.com>
2025-04-23 15:24:07 +0200

    update product card

--- components/productCard.vue
+++ components/productCard.vue
@@ -11,68 +11,69 @@
     };
 </script>
 
-    <template>
-        <div class="relative transition-all duration-500 frame group hover:-translate-y-3">
-            <div
-                class="card flex flex-col justify-between text-center relative transition-shadow duration-500 group-hover:shadow-[0px_10px_15px_0px_#0000001A]">
-                <div>
-                    <div class="flex flex-row items-center justify-between">
-                        <div v-if="useFavoritesStore().isFavorite(props.product)">
-                            <Icon @click="useFavoritesStore().removeFavoriteProduct(props.product)"
-                                name="material-symbols:favorite" class="text-blue-500 icon-xl" />
-                        </div>
-                        <div v-else>
-                            <Icon @click="useFavoritesStore().addFavoriteProduct(props.product)"
-                                name="material-symbols:favorite-outline" class="text-gray-900 icon-xl" />
-                        </div>
-                        <p id="tag"
-                            class="px-3 py-2 font-semibold text-white bg-blue-300 rounded-lg tags font-saira text-label1 radius">
-                            NOVO</p>
+<template>
+    <div class="relative transition-all duration-500 frame group hover:-translate-y-3">
+        <div
+            class="card flex flex-col justify-between text-center relative transition-shadow duration-500 group-hover:shadow-[0px_10px_15px_0px_#0000001A]">
+            <div>
+                <div class="flex flex-row items-center justify-between">
+                    <div v-if="useFavoritesStore().isFavorite(props.product)">
+                        <Icon @click="useFavoritesStore().removeFavoriteProduct(props.product)"
+                            name="material-symbols:favorite" class="text-blue-500 icon-xl" />
                     </div>
-
-                    <img :src="product.gallery[0]" alt="product thumb" class="max-h-[320px] max-w-[85%] mx-auto">
+                    <div v-else>
+                        <Icon @click="useFavoritesStore().addFavoriteProduct(props.product)"
+                            name="material-symbols:favorite-outline" class="text-gray-900 icon-xl" />
+                    </div>
+                    <p id="tag"
+                        class="px-3 py-2 font-semibold text-white bg-blue-300 rounded-lg tags font-saira text-label1 radius">
+                        NOVO</p>
                 </div>
 
-                <div class="flex flex-row justify-center gap-2 pt-6">
-                    <p class="font-bold text-blue-900 font-saira text-h6-normal">{{
-                        product.price.toFixed(2).replace('.', ',') }} €</p>
-                    <span class="font-bold text-blue-900 font-saira text-h6-normal">|</span>
-                    <p class="font-bold text-blue-500 font-saira text-h6-normal ">{{
-                        product.member_price.toFixed(2).replace('.', ',') }} €</p>
-                    <UPopover :popper="{ placement: 'top-start' }"
-                        :ui="{ ring: 'ring-0', background: 'dark:bg-blue-50 bg-blue-50' }">
-                        <UButton trailing-icon="mynaui:info-hexagon"
-                            class="bg-white shadow-none hover:bg-white text-blue-500 p-0 mt-[2px]" />
-
-                        <template #panel>
-                            <div class="px-3 py-2">
-                                <p class="font-bold text-gray-900 font-roboto text-body4">Cijena za članove</p>
-                            </div>
-                        </template>
-                    </UPopover>
-                </div>
-
-                <div class="transition-opacity duration-300 opacity-0 middle group-hover:opacity-100">
-                    <NuxtLink :to="`/products/${product.id}`"
-                        class="font-semibold btn-primary large active:bg-blue-800">ODABERI OPCIJE
-                    </NuxtLink>
-
-                    <UButton v-if="product.variations?.length < 1" @click="addToCart" size="lg" variant="solid" :ui="{
-                        base: 'text-white font-saira font-semibold',
-                        variant: {
-                            solid: 'bg-blue-600 hover:bg-blue-700 active:bg-800'
-                        }
-                    }"
-                        class="text-[15px] flex justify-center focus:outline-none focus:ring-0 active:bg-blue-800 transition-colors duration-200 font-saira font-semibold ">
-                        DODAJ U KOŠARICU
-                    </UButton>
-
-                </div>
+                <img :src="product.gallery[0]" alt="product thumb" class="max-h-[320px] max-w-[85%] mx-auto">
             </div>
-            <p class="p-2 mt-2 font-semibold text-center text-blue-900 font-saira text-h5-normal">{{ product.title }}
-            </p>
+
+            <div class="flex flex-row justify-center gap-2 pt-6">
+                <p class="font-bold text-blue-900 font-saira text-h6-normal">{{
+                    product.price.toFixed(2).replace('.', ',') }} €</p>
+                <span class="font-bold text-blue-900 font-saira text-h6-normal">|</span>
+                <p class="font-bold text-blue-500 font-saira text-h6-normal ">{{
+                    product.member_price.toFixed(2).replace('.', ',') }} €</p>
+                <UPopover :popper="{ placement: 'top-start' }"
+                    :ui="{ ring: 'ring-0', background: 'dark:bg-blue-50 bg-blue-50' }">
+                    <UButton trailing-icon="mynaui:info-hexagon"
+                        class="bg-white shadow-none hover:bg-white text-blue-500 p-0 mt-[2px]" />
+
+                    <template #panel>
+                        <div class="px-3 py-2">
+                            <p class="font-bold text-gray-900 font-roboto text-body4">Cijena za članove</p>
+                        </div>
+                    </template>
+                </UPopover>
+            </div>
+
+            <div class="transition-opacity duration-300 opacity-0 middle group-hover:opacity-100">
+                <NuxtLink v-if="product.variations?.length > 0" :to="`/products/${product.id}`"
+                    class="font-semibold btn-primary large active:bg-blue-800">
+                    ODABERI OPCIJE
+                </NuxtLink>
+
+                <UButton v-if="product.variations?.length < 1" @click="addToCart" size="lg" variant="solid" :ui="{
+                    base: 'text-white font-saira font-semibold',
+                    variant: {
+                        solid: 'bg-blue-600 hover:bg-blue-700 active:bg-800'
+                    }
+                }"
+                    class="text-[15px] flex justify-center focus:outline-none focus:ring-0 active:bg-blue-800 transition-colors duration-200 font-saira font-semibold ">
+                    DODAJ U KOŠARICU
+                </UButton>
+
+            </div>
         </div>
-    </template>
+        <p class="p-2 mt-2 font-semibold text-center text-blue-900 font-saira text-h5-normal">{{ product.title }}
+        </p>
+    </div>
+</template>
 
 <style scoped>
 .middle {
