377245c3e171fa7c7672ce29dced89232639d4ea
ivana-p1nk <iicetina29@gmail.com>
2025-05-07 13:45:12 +0200

    pagination test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,10 +18,12 @@ export default defineNuxtConfig({
         },
     },
 
+    /*
     devServer: {
         host: 'web.nkrijeka-app.test',
         port: 3000,
     },
+    */
 
     runtimeConfig: {
         public: {
