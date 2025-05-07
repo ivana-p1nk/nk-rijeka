701236f2048bc684f7f9f437fbd5a7f556dc10a2
kristijanw <freelance.kristijan@gmail.com>
2025-05-07 15:45:27 +0200

    update web

--- pages/categories/[catslug]/index.vue
+++ pages/categories/[catslug]/index.vue
@@ -5,7 +5,7 @@ import { useRoute, createError } from '#imports'
 const route = useRoute()
 const catslug = route.params.catslug as string
 
-const { category, products, filteredProducts, activeFilters, loadingCat, fetchAll, sort } = useCategoryProducts(catslug)
+const { category, products, filteredProducts, activeFilters, loadingCat, fetchAll, sort, page, totalPages } = useCategoryProducts(catslug)
 
 await fetchAll()
 
@@ -20,15 +20,9 @@ if (!category.value) {
 <template>
     <div class="bg-igraci">
         <div class="container mx-auto con-height pb-5 px-5" v-if="!loadingCat && category">
-            <CategoryPage
-                :category="category"
-                :products="products"
-                :filtered-products="filteredProducts"
-                :active-filters="activeFilters"
-                :sort="sort"
-                @update:activeFilters="activeFilters = $event"
-                @update:sort="sort = $event"
-            />
+            <CategoryPage :category="category" :products="products" :filtered-products="filteredProducts"
+                :active-filters="activeFilters" :sort="sort" @update:activeFilters="activeFilters = $event" :page="page"
+                :total-pages="totalPages" @update:sort="sort = $event" @update:page="page = $event" />
         </div>
     </div>
 </template>
--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -1,52 +1,56 @@
 <script setup lang="ts">
-	import type { ICategory } from '~/types/category';
-	const config = useRuntimeConfig();
+import type { ICategory } from '~/types/category';
+const config = useRuntimeConfig();
 
-	const loading = ref(true);
-    const categories = ref<ICategory[]>();
+const loading = ref(true);
+const categories = ref<ICategory[]>();
 
-	const fetchCategories = async () => {
-        loading.value = true;
+const fetchCategories = async () => {
+	loading.value = true;
 
-        try {
-            const { data: categoryData } = await useFetch(`${config.public.url}/categories`);
-            // @ts-ignore
-            categories.value = categoryData.value;
-            loading.value = false;
-        } catch (error) {
-            console.error(error);
-            loading.value = false;
-        }
-    }
+	try {
+		const { data: categoryData } = await useFetch(`${config.public.url}/categories`);
+		// @ts-ignore
+		categories.value = categoryData.value;
+		loading.value = false;
+	} catch (error) {
+		console.error(error);
+		loading.value = false;
+	}
+}
 
-    onMounted(fetchCategories);
+onMounted(fetchCategories);
 </script>
 
 <template>
 	<div class="bg-igraci">
-    <div class="container mx-auto con-height pb-5 px-5">
-		<p class="font-normal text-blue-900 font-roboto text-body2 ">
-			<NuxtLink class="text-blue-400 link-color" to="/">Početna / </NuxtLink> 
-			<span>Kategorije</span>
-        </p>
-		
-		<p class="pt-1 pb-8 text-h1-normal font-medium uppercase text-blue-900 font-saira border-b border-1 border-gray-200">
-        SVE KATEGORIJE
-        </p>
+		<div class="container mx-auto con-height pb-5 px-5">
+			<p class="font-normal text-blue-900 font-roboto text-body2 ">
+				<NuxtLink class="text-blue-400 link-color" to="/">Početna / </NuxtLink>
+				<span>Kategorije</span>
+			</p>
 
-		<ul class="text-blue-400 underline pt-10">
-			<li v-for="category in categories" :key="category.id">
-				<NuxtLink :to="`/categories/${encodeURIComponent(category.slug!)}`">{{ category.title }}</NuxtLink>
-				<ul v-if="category.sub_categories && category.sub_categories.length">
-					<li v-for="subcategory in category.sub_categories" :key="subcategory.id" class="ml-4">
-						<NuxtLink :to="`/categories/${encodeURIComponent(category.slug!)}/${encodeURIComponent(subcategory.slug!)}`">
-							{{ subcategory.title }}
-						</NuxtLink>
-					</li>
-				</ul>
-			</li>
-		</ul>
+			<p
+				class="pt-1 pb-8 text-h1-normal font-medium uppercase text-blue-900 font-saira border-b border-1 border-gray-200">
+				SVE KATEGORIJE
+			</p>
+
+			<ul class="text-blue-400 underline pt-10">
+				<li v-for="category in categories" :key="category.id">
+					<NuxtLink :to="`/categories/${encodeURIComponent(category.slug!)}`">
+						{{ category.title }} ({{ category.total_products_count }})
+					</NuxtLink>
+					<ul v-if="category.sub_categories && category.sub_categories.length">
+						<li v-for="subcategory in category.sub_categories" :key="subcategory.id" class="ml-4">
+							<NuxtLink
+								:to="`/categories/${encodeURIComponent(category.slug!)}/${encodeURIComponent(subcategory.slug!)}`">
+								{{ subcategory.title }}
+							</NuxtLink>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
 	</div>
-</div>
 
 </template>
