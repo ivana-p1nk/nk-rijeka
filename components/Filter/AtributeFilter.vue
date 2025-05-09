a45929444a060e283c067a6710a489f28bc7ab5f
kristijanw <freelance.kristijan@gmail.com>
2025-05-09 13:18:01 +0200

    update cart

--- components/Filter/AtributeFilter.vue
+++ components/Filter/AtributeFilter.vue
@@ -1,112 +1,108 @@
 <template>
   <div v-for="filter in filterConfigs" :key="filter.key" class="w-full sm:w-48">
-    <USelect  class="w-full sm:w-48"
-      v-model="filters[filter.key as FilterKey]"
-      :options="filter.options"
-
-      :ui="selectUI"
-    />
+    <USelect class="w-full sm:w-48" v-model="filters[filter.key as FilterKey]" :options="filter.options"
+      :ui="selectUI" />
   </div>
 </template>
-  
-<script setup lang="ts">
-  import { computed } from 'vue';
-  import type { IProduct } from '~/types/product';
 
-  const props = defineProps<{
-    products: IProduct[];
-    filters: Record<string, string>
-  }>();
-    
-  const emit = defineEmits<{
-    (e: 'update:filters', value: Record<FilterKey, string>): void;
-  }>();
-    
-  type FilterKey = 'size' | 'gender' | 'age' | 'price';
-    
-  const filters = reactive<Record<FilterKey, string>>({
-    size: '',
-    gender: '',
-    age: '',
-    price: ''
-  });
-    
-  watch(filters, () => {
-    emit('update:filters', { ...filters });
-  }, { deep: true });
-    
-  const allProducts = computed(() => props.products);
-    
-  const availableVariations = computed(() => {
+<script setup lang="ts">
+import { computed } from 'vue';
+import type { IProduct } from '~/types/product';
+
+const props = defineProps<{
+  products: IProduct[];
+  filters: Record<string, string>
+}>();
+
+const emit = defineEmits<{
+  (e: 'update:filters', value: Record<FilterKey, string>): void;
+}>();
+
+type FilterKey = 'size' | 'gender' | 'age' | 'price';
+
+const filters = reactive<Record<FilterKey, string>>({
+  size: '',
+  gender: '',
+  age: '',
+  price: ''
+});
+
+watch(filters, () => {
+  emit('update:filters', { ...filters });
+}, { deep: true });
+
+const allProducts = computed(() => props.products);
+
+const availableVariations = computed(() => {
   const allVariations = allProducts.value
     .flatMap(p => p.variations || [])
     .map(v => v.packaging);
 
-    return Array.from(new Set(allVariations));
-  });
- 
-  const availablePriceRanges = computed(() => {
-    const ranges = ['0-15', '15-30', '30-50', '50-100', '100-150', '150-200', '200+'];
+  return Array.from(new Set(allVariations));
+});
 
-    return ranges.map(range => ({
-      label: `${range}€`,
-      value: range
-    }));
-  });
+const availablePriceRanges = computed(() => {
+  const ranges = ['0-15', '15-30', '30-50', '50-100', '100-150', '150-200', '200+'];
 
-  /*Konfiguracija USelect komponente*/
-  const filterConfigs = computed(() => {
-    const configs = [];
+  return ranges.map(range => ({
+    label: `${range}€`,
+    value: range
+  }));
+});
 
-    if (availableVariations.value.length > 0) {
-      configs.push({
-        key: 'size',
-        label: 'Veličina',
-        options: [
-          { label: 'Veličina', value: '' },
-          ...availableVariations.value
-        ]
-      });
+/*Konfiguracija USelect komponente*/
+const filterConfigs = computed(() => {
+  const configs = [];
+
+  if (availableVariations.value.length > 0) {
+    configs.push({
+      key: 'size',
+      label: 'Veličina',
+      options: [
+        { label: 'Veličina', value: '' },
+        ...availableVariations.value
+      ]
+    });
+  }
+
+  configs.push(
+    {
+      key: 'gender',
+      label: 'Spol',
+      options: [
+        { label: 'Spol', value: '' },
+        { label: 'Muškarci', value: 'muskarci' },
+        { label: 'Žene', value: 'zene' }
+      ]
+    },
+    {
+      key: 'age',
+      label: 'Dob',
+      options: [
+        { label: 'Dob', value: '' },
+        { label: 'Odrasli', value: 'odrasli' },
+        { label: 'Djeca', value: 'djeca' }
+      ]
     }
+  );
 
-    configs.push(
-      {
-        key: 'gender',
-        label: 'Spol',
-        options: [
-          { label: 'Spol', value: '' },
-          { label: 'Muškarci', value: 'muskarci' },
-          { label: 'Žene', value: 'zene' }
-        ]
-      },
-      {
-        key: 'age',
-        label: 'Dob',
-        options: [
-          { label: 'Dob', value: '' },
-          { label: 'Odrasli', value: 'odrasli' },
-          { label: 'Djeca', value: 'djeca' }
-        ]
-      }
-    );
+  if (availablePriceRanges.value.length > 0) {
+    configs.push({
+      key: 'price',
+      label: 'Cijena',
+      options: [
+        { label: 'Cijena', value: '' },
+        ...availablePriceRanges.value
+      ]
+    });
+  }
+  return configs;
+});
 
-    if (availablePriceRanges.value.length > 0) {
-  configs.push({
-    key: 'price',
-    label: 'Cijena',
-    options: [
-      { label: 'Cijena', value: '' },
-      ...availablePriceRanges.value
-    ]
-  });
-}
-    return configs;
-  });
-
-  const selectUI = {
-    color: {
-      white: {
-        outline: `
+const selectUI = {
+  color: {
+    white: {
+      outline: `
           border border-gray-300
           bg-white
           text-black
@@ -119,18 +115,18 @@
           focus:ring-0
           focus:ring-blue-500
           `
-      }
     }
-  };
-  
+  }
+};
+
 </script>
-  
+
 <style>
-  .form-select {
-    @apply text-black placeholder-black !important;
-  }
-  .dark .form-select {
-    @apply text-black placeholder-black !important;
-  }
+.form-select {
+  @apply text-black placeholder-black !important;
+}
+
+.dark .form-select {
+  @apply text-black placeholder-black !important;
+}
 </style>
-  
