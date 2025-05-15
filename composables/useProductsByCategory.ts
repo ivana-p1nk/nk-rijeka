892036165120a2e7454bf83c0e652d0478ae5507
ivana-p1nk <iicetina29@gmail.com>
2025-05-15 12:42:10 +0200

    Test

--- composables/useProductsByCategory.ts
+++ composables/useProductsByCategory.ts
@@ -1,7 +1,7 @@
 import { ref } from 'vue'
 import type { IProduct } from '~/types/product'
 
-export const useProductsByCategory = (categoryId: number) => {
+export const useProductsByCategory = (categoryId: Ref<number>) => {
   const config = useRuntimeConfig()
   const products = ref<IProduct[]>([])
   const loading = ref(false)
@@ -10,19 +10,21 @@ export const useProductsByCategory = (categoryId: number) => {
     loading.value = true
     try {
       const { data: productData } = await useFetch(`${config.public.url}/products`, {
-        params: { categories: categoryId },
+        params: { categories: categoryId.value },
       })
 
       // @ts-ignore
       products.value = productData.value.data || []
     } catch (error) {
-      console.error(`Greška kod učitavanja proizvoda za kategoriju ${categoryId}:`, error)
+      console.error(`Greška kod učitavanja proizvoda za kategoriju ${categoryId.value}:`, error)
     } finally {
       loading.value = false
     }
   }
 
-  onMounted(fetchProducts)
+   watch(categoryId, () => {
+    fetchProducts()
+  }, { immediate: true })
 
   return { products, loading, fetchProducts }
 }
