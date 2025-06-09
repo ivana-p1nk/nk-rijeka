ffb7aef232ee63994e96121dc5e6c57013b45afb
ivana-p1nk <iicetina29@gmail.com>
2025-06-09 11:22:28 +0200

    Succes registration page responsive and fixes

--- pages/succes-registration.vue
+++ pages/succes-registration.vue
@@ -1,120 +1,28 @@
 <script setup lang="ts">
-import { z } from 'zod'
-import type { FormSubmitEvent } from '#ui/types'
+    const router = useRouter()
 
-type Schema = z.output<typeof schema>
+    definePageMeta({
+        layout: false,
+    })
 
-const { api } = useAxios()
-const router = useRouter()
-const toast = useToast()
+    useSeoMeta({
+        title: 'Uspješna registracija',
+    })
 
-definePageMeta({
-    layout: false,
-})
-
-useSeoMeta({
-    title: 'Registracija',
-})
-
-const schema = z.object({
-    email: z.string({ message: 'Ovo polje je obavezno!' }),
-    name: z.string({ message: 'Ovo polje je obavezno!' }),
-    password: z.string({ message: 'Ovo polje je obavezno!' }),
-    password_confirmation: z.string({ message: 'Ovo polje je obavezno!' }),
-})
-
-const state = reactive({
-    barcode: undefined,
-    name: undefined,
-    email: undefined,
-    password: undefined,
-    password_confirmation: undefined,
-})
-
-const errors = ref({})
-const loading = ref(false)
-
-async function onSubmit(event: FormSubmitEvent<Schema>) {
-    loading.value = true
-
-    console.log(state)
-
-    if (state.password !== state.password_confirmation) {
-        toast.add({
-            color: 'red',
-            title: 'Greška',
-            description: 'Lozinke se ne podudaraju.',
-            timeout: 2500,
-        })
-        loading.value = false
-        return
+    const goToPreviousPage = () => {
+        router.go(-1)
     }
-
-    api.post('/register', state)
-        .then(({ data }) => {
-            loading.value = false
-
-            if (data.status === 'error') {
-                errors.value = data.errors
-                toast.add({
-                    color: 'red',
-                    title: 'Greška',
-                    description: data.message,
-                    timeout: 2500,
-                })
-                return
-            }
-
-            if (data === 'ok') {
-                errors.value = <
-                    {
-                        email: string
-                        name: string
-                        password: string
-                        password_confirmation: string
-                    }
-                >{}
-                toast.add({
-                    title: 'Uspešno ste se registrirali',
-                    timeout: 2500,
-                })
-                navigateTo('/login')
-            }
-        })
-        .catch((err) => {
-            loading.value = false
-            errors.value = err.response.data.errors
-            toast.add({
-                color: 'red',
-                title: 'Greška',
-                description: 'Nešto nije kako treba, javite se korisničkoj podršci.',
-                timeout: 2500,
-            })
-        })
-}
-
-const typePass = ref('password')
-const showPassword = () => {
-    typePass.value = 'text'
-}
-const hidePassword = () => {
-    typePass.value = 'password'
-}
-
-const goToPreviousPage = () => {
-    router.go(-1)
-}
 </script>
 
 <template>
-    <section id="form" aria-labelledby="form" class="container mx-auto justify-center pt-20 pb-32">
+    <section class="container mx-auto justify-center pt-20 pb-32 px-5">
         <div class="absolute top-7 left-7">
             <button @click="goToPreviousPage" class="uppercase btn-secondary py-3 xs flex items-center w-fit h-fit">
                 <Icon name="i-heroicons-chevron-left" />
             </button>
         </div>
 
-        <div class="pb-12 mx-auto">
+        <div class="pb-8 mx-auto">
             <NuxtLink to="/">
                 <img src="/assets/images/grbrijeka.svg" alt="logo" class="w-20 h-20 mx-auto" />
             </NuxtLink>
@@ -124,10 +32,10 @@ const goToPreviousPage = () => {
 
         </div>
 
-        <div class="text-center max-w-[376px] mx-auto">
+        <div class="text-center max-w-[400px] mx-auto">
             <img src="/assets/images/racun-kreiran.jpg" class="w-full rounded-md">
-            <p class="px-5 text-body2 font-bold text-blue-900 py-4">Kako bi završili registraciju vašeg računa aktivirajte link koji je poslan na vašu email adresu.</p>
-            <p class="px-5 text-body3 text-blue-900 pb-4">Lozinku i email adresu možete promjeniti u postavkama računa</p>
+            <p class="px-0 sm:px-5 text-body2 font-bold text-blue-900 pt-8 pb-4">Kako bi završili registraciju vašeg računa aktivirajte link koji je poslan na vašu email adresu.</p>
+            <p class="px-0 sm:px-5 text-body3 text-blue-900 pb-4">Lozinku i email adresu možete promjeniti u postavkama računa</p>
             <hr class="border-t-[1.4px] border-gray-200" />
             <p class="px-5 text-body3 font-bold text-blue-500 py-4">Hvala Vam na podršci kluba</p>
             <NuxtLink
