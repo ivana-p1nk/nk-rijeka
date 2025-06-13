332ad4890082c19e01855af6482c54dc925147bd
ivana-p1nk <iicetina29@gmail.com>
2025-06-13 15:14:27 +0200

    Change color of tags

--- composables/useProductTags.ts
+++ composables/useProductTags.ts
@@ -5,10 +5,10 @@ export const useProductTags = (product: IProduct) => {
   const categoryIds = product.category?.map(cat => cat.id) ?? []
 
   if (categoryIds.includes(34)) {
-    tags.push({ label: 'NOVO', color: 'bg-blue-500' })
+    tags.push({ label: 'NOVO', color: 'bg-blue-300' })
   }
   if (categoryIds.includes(8)) {
-    tags.push({ label: 'AKCIJA', color: 'bg-red-500' })
+    tags.push({ label: 'AKCIJA', color: 'bg-red-600' })
   }
 
   return tags
