7741a4da852becb692533effa5fa7b80e9a338a1
ivana-p1nk <iicetina29@gmail.com>
2025-08-11 09:14:48 +0200

    GTM fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,21 +16,20 @@ export default defineNuxtConfig({
                 })(window, document, "clarity", "script", "s5ywisqnf3");`,
                 type: 'text/javascript'
                 },
-             // Google Tag Manager
+                // Google Tag Manager
                 {
                     hid: 'gtm-script',
-                    children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                     })(window,document,'script','dataLayer','GTM-PRVJ4LG9');`,
-                    type: 'text/javascript'
                 }
             ],
             noscript: [
                 {
                     hid: 'gtm-noscript',
-                    children: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PRVJ4LG9"
+                    innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PRVJ4LG9"
                         height="0" width="0" style="display:none;visibility:hidden"></iframe>`
                 }
             ]
